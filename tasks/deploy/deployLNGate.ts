c1c89aee8315cb7b211bd5830ee6952b732831dd
OxGAB <oxcryptogab@gmail.com>
2023-06-05 08:25:52 +0300

    longnecks mock

--- tasks/deploy/deployLNGate.ts
+++ tasks/deploy/deployLNGate.ts
@@ -1,6 +1,12 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { CANTO_LONG_NECKS_ADDRESS, LZ_OPTIONS, ChainID } from './constants';
+import {
+    CANTO_LONG_NECKS_ADDRESS,
+    LZ_OPTIONS,
+    ChainID,
+    LONG_NECKS_BASE_URI,
+} from './constants';
 import { saveDeployment } from '../utils';
+import inquirer from 'inquirer';
 // TODO: Create a deployment logging system
 export const deployLNGate__task = async (
     args: any,
@@ -19,11 +25,39 @@ export const deployLNGate__task = async (
             'Long Necks Gate can only be deployed on the Canto network',
         );
     }
+
+    let longNecksAddr = CANTO_LONG_NECKS_ADDRESS;
+
+    if (
+        chainName.toLowerCase() === 'canto_testnet' &&
+        (await inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'confirm',
+                message: `Are you sure you want to deploy the Long Necks ERC721 Mock on ${chainName}?`,
+            },
+        ]))
+    ) {
+        const longNecksFactory = await ethers.getContractFactory('ERC721Mock');
+        const longNecks = await longNecksFactory.deploy('Long Necks', 'LN');
+        longNecksAddr = longNecks.address;
+        await longNecks.deployed();
+        await longNecks.setBaseURI(LONG_NECKS_BASE_URI);
+        saveDeployment(chainID, [
+            {
+                ERC721Mock: {
+                    address: longNecks.address,
+                    deploymentTxHash: longNecks.deployTransaction.hash,
+                    constructorArgs: ['Long Necks', 'LN'],
+                },
+            },
+        ]);
+    }
     const lnGateFactory = await ethers.getContractFactory(
         'LongOmnichainNeckGate',
     );
     const lnGate = await lnGateFactory.deploy(
-        CANTO_LONG_NECKS_ADDRESS,
+        longNecksAddr,
         LZ_OPTIONS[chainID]!.lzEndpointAddr,
         LZ_OPTIONS[chainID]!.minGas,
     );
