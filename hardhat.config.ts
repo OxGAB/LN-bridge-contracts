b6c78c6a294853995763ddb2d40caeb5146b4d97
OxGAB <oxcryptogab@gmail.com>
2023-06-22 23:33:34 +0300

    add tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,9 @@ import '@nomicfoundation/hardhat-toolbox';
 import { deployLNGate__task } from './tasks/deploy/deployLNGate';
 import { deployLNONFT__task } from './tasks/deploy/deployLNONFT';
 import { setTrustedRemotes__task } from './tasks/execs/setTrustedRemote';
+import { mintLN__task } from './tasks/execs/mintLN';
+import { sendFrom__task } from './tasks/execs/sendFrom';
+import { setPacketType__task } from './tasks/execs/setPacketType';
 dotenv.config();
 task('deployLNGate', 'Deploy LongNecksGate contract').setAction(
     deployLNGate__task,
@@ -20,6 +23,18 @@ task('deployLNONFT', 'Deploy an ONFT contract').setAction(deployLNONFT__task);
 task('setTrustedRemotes', 'Set trusted remotes for contract on a certain chain')
     .setAction(setTrustedRemotes__task)
     .addParam('remotes', 'The remotes to set');
+task('mintLN', 'Mint Long Necks')
+    .setAction(mintLN__task)
+    .addOptionalParam('number', 'The number of Long Necks to mint')
+    .addOptionalParam('to', 'The address to mint to');
+task('sendFrom', 'Send Long Necks from one chain to another')
+    .addParam('dstchainid', 'The destination chain ID')
+    .addOptionalParam('to', 'The address to send to')
+    .addOptionalParam('tokenids', 'The token IDs to send')
+    .setAction(sendFrom__task);
+task('setPacketType', 'Set packet type for a chain')
+    .addParam('chainids', 'The chain IDs to set')
+    .setAction(setPacketType__task);
 const config: HardhatUserConfig = {
     solidity: {
         compilers: [
