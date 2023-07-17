946a1ff69785a5a5fb50ac33176471cc3cc14ede
OxGAB <oxcryptogab@gmail.com>
2023-07-17 23:43:40 +0300

    lnGate `setApprovalForAll` test

--- test/lnGate.test.ts
+++ test/lnGate.test.ts
@@ -44,4 +44,44 @@ describe('LongOmnichainNeckGate tests', () => {
         ).to.emit(lnGate, 'LongOmnichainNeckGate__RescuedToken');
         expect(await lnMock.ownerOf(tokenId)).to.equal(lnOwner.address);
     });
+    it('Should fail if a user tries to teleport for another user', async () => {
+        const { lnMock, lnGate, lnOwner, deployer, chainIdEth, lnONFT } =
+            await loadFixture(register);
+        const tokenId = 1;
+        const defaultAdapterParams = ethers.utils.solidityPack(
+            ['uint16', 'uint256'],
+            [1, 250000],
+        );
+        // estimate nativeFees
+        const nativeFee = (
+            await lnGate.estimateSendFee(
+                chainIdEth,
+                lnOwner.address,
+                tokenId,
+                false,
+                defaultAdapterParams,
+            )
+        ).nativeFee;
+        await lnMock.connect(lnOwner).setApprovalForAll(lnGate.address, true);
+        await expect(
+            lnGate
+                .connect(deployer)
+                .sendFrom(
+                    lnOwner.address,
+                    chainIdEth,
+                    deployer.address,
+                    tokenId,
+                    lnOwner.address,
+                    ethers.constants.AddressZero,
+                    defaultAdapterParams,
+                    {
+                        value: nativeFee,
+                    },
+                ),
+        ).to.be.revertedWithCustomError(
+            lnGate,
+            'LongOmnichainNeckGate__NotOwner',
+        );
+        expect(await lnMock.ownerOf(tokenId)).to.equal(lnOwner.address);
+    });
 });
